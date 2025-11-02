c088aae8dc8945f3477cc929304792e469e69289
Andrei Vilcu <andrei@mindfuldesign.ro>
2025-11-02 12:32:22 +0200

    Add conditional alerts to housing status

--- src/products/benefits-calculator/where-you-live.tsx
+++ src/products/benefits-calculator/where-you-live.tsx
@@ -3,6 +3,7 @@ import { z } from 'zod'
 import { useAppForm } from '~/components/Form/use-app-form'
 import { Page } from '~/products/shared/Page'
 import { useWorkflow } from '../shared/use-workflow'
+import { Alert } from '~/components/Alert'
 
 const HOUSING_STATUS_OPTIONS = [
   {
@@ -90,6 +91,64 @@ export function WhereYouLive() {
               <field.RadioField
                 label="Which best describes your current housing status?"
                 options={HOUSING_STATUS_OPTIONS}
+                descriptionAfter={
+                  <>
+                    {field.state.value === 'SupportedAccommodation' && (
+                      <Alert type="info">
+                        If your landlord provides you with support services, supervision or care
+                        then your home may be classified as supported accommodation. Various
+                        exemptions from welfare reforms, such as the benefit cap, apply to people
+                        living in supported accommodation. If you are in doubt about your housing
+                        status please read our information on supported exempt accommodation and/or
+                        check with your landlord.
+                      </Alert>
+                    )}
+
+                    {field.state.value === 'TemporaryAccommodation' && (
+                      <Alert type="info">
+                        This is the correct option if your council has placed you in temporary
+                        accommodation while they find alternative accommodation for you. Please
+                        enter the postcode of where you are staying at the moment.
+                      </Alert>
+                    )}
+
+                    {field.state.value === 'BoarderOrLodger' && (
+                      <Alert type="info">
+                        Select this option if you pay rent to someone who also lives in the
+                        accommodation and you are not related to them or staying there as a friend.
+                      </Alert>
+                    )}
+
+                    {field.state.value === 'LivingWithParents' && (
+                      <Alert type="info">
+                        Select this option if you live with a relative or friend on an informal
+                        basis, without a contract to pay rent. You may have an informal arrangement
+                        to pay money to stay there, but you do not have a formal contract to pay
+                        rent or invoices for rent paid.
+                      </Alert>
+                    )}
+
+                    {field.state.value === 'Homeless' && (
+                      <Alert type="info">
+                        You count as homeless if you have no regular place to stay. You may be
+                        staying in different places or on the street. If you don’t have a regular
+                        postcode please instead select the Local Authority area where you stay most
+                        often.
+                      </Alert>
+                    )}
+
+                    {field.state.value === 'InPrisonOrInHospital' && (
+                      <Alert type="info">
+                        You have told us you are in prison or in hospital at the moment, but we want
+                        to work out the benefits you will be entitled to when you are back in the
+                        community. If you have nowhere to live back in the community you are most
+                        likely to look for accommodation in the private rented sector. Accordingly,
+                        we will ask you about the rent and Council Tax you expect to pay and will
+                        assume that you are renting privately.
+                      </Alert>
+                    )}
+                  </>
+                }
               />
             )}
           />
