b493c1450089dba5149e5d585014ec77032ba996
Andrei Vilcu <andrei@mindfuldesign.ro>
2025-11-03 23:13:05 +0200

    Update form default values

--- src/products/benefits-calculator/results.tsx
+++ src/products/benefits-calculator/results.tsx
@@ -3,90 +3,9 @@ import { Page } from '~/products/shared/Page'
 import { Alert } from '~/components/Alert'
 import { useWorkflow } from '../shared/use-workflow'
 import { UniversalCreditCalculator } from './utils/calculator'
-import { benefitCalculator } from './utils/benefitCalculator'
 import { getPensionAgeWarningType } from './utils/pensionAgeCalculator'
 
-const DEFAULT_VALUES = {
-  // Tax Year and Circumstances
-  taxYear: '2025_26',
-  circumstances: 'single',
-  age: 25,
-  partnerAge: 25,
-
-  // Housing
-  housingStatus: 'no_housing_costs',
-  tenantType: 'social',
-  brma: '',
-  rent: 0,
-  rentPeriod: 'per_month',
-  serviceCharges: 0,
-  serviceChargesPeriod: 'per_month',
-  bedrooms: 1,
-  nonDependants: 0,
-
-  // Employment and Disability - Main Person
-  employmentType: 'not_working',
-  monthlyEarnings: 0,
-  monthlyEarningsPeriod: 'per_month',
-  pensionAmount: 0,
-  pensionAmountPeriod: 'per_month',
-  pensionType: 'amount',
-  pensionPercentage: 3,
-  isDisabled: 'no',
-  claimsDisabilityBenefits: 'no',
-  disabilityBenefitType: '',
-  pipDailyLivingRate: 'none',
-  pipMobilityRate: 'none',
-  dlaCareRate: 'none',
-  dlaMobilityRate: 'none',
-  aaRate: 'none',
-  hasLCWRA: 'no',
-
-  // Employment and Disability - Partner
-  partnerEmploymentType: 'not_working',
-  partnerMonthlyEarnings: 0,
-  partnerMonthlyEarningsPeriod: 'per_month',
-  partnerPensionAmount: 0,
-  partnerPensionAmountPeriod: 'per_month',
-  partnerPensionType: 'amount',
-  partnerPensionPercentage: 3,
-  partnerIsDisabled: 'no',
-  partnerClaimsDisabilityBenefits: 'no',
-  partnerDisabilityBenefitType: '',
-  partnerPipDailyLivingRate: 'none',
-  partnerPipMobilityRate: 'none',
-  partnerDlaCareRate: 'none',
-  partnerDlaMobilityRate: 'none',
-  partnerAaRate: 'none',
-  partnerHasLCWRA: 'no',
-
-  // Children
-  hasChildren: false,
-  children: 0,
-  childAges: [],
-  childDisabilities: [],
-  childGenders: [],
-  childcareCosts: 0,
-  childcareCostsPeriod: 'per_month',
-
-  // Carer Status
-  isCarer: 'no',
-  isPartnerCarer: 'no',
-
-  // Other Benefits
-  hasOtherBenefits: 'no',
-  otherBenefitsList: [],
-
-  // Savings
-  hasSavings: 'no',
-  hasSavingsOver6000: 'no',
-  savingsAmount: 0,
-  savings: 0,
-  savingsPeriod: 'per_month',
-
-  // Area
-  area: 'england',
-}
+import { DEFAULT_VALUES } from './constants'
 
 export function Results() {
   const { entry } = useWorkflow()
