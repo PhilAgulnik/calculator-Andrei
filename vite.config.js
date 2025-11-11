e2015e2eb3260eb2629a4777a92d027be5389d02
denysdenysenko <d.denysenko@ukad-group.com>
2025-11-11 10:42:17 +0200

    Module resolution update

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,23 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import checker from 'vite-plugin-checker'
 import react from '@vitejs/plugin-react'
 import tsConfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
+import { fileURLToPath } from 'url'
 
-import { APP_BASE_PATH } from './constants'
+import { APP_BASE_PATH } from './constants.ts'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 export default defineConfig({
   server: {
     port: 3000,
   },
   base: APP_BASE_PATH,
+  resolve: {
+    alias: {
+      '~': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [
     checker({
       typescript: true,
